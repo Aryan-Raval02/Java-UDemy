f04b503259cd1ac74c03ee012e8c9deffaf3ceb5
deva964 <aryanraval299@gmail.com>
2025-05-09 17:17:12 +0530

    Introduction of JAVA

---
+++ 1. Introduction/ReadKeyboard.java
@@ -0,0 +1,33 @@
+// When Scanner class is not imported then compile time error "Cannot find Symbol"
+
+//When we are providing Unexpected input then it will show RE : "input Mismatch Exception"
+import java.util.Scanner;
+
+public class ReadKeyboard {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.print("Enter A String : ");
+//        String s = sc.nextLine();
+
+//        System.out.print("Enter A Number : ");
+//        int a = sc.nextInt();
+//        System.out.println();
+
+//        System.out.print("Enter Floating Number : ");
+//        float f = sc.nextFloat();
+//        System.out.println();
+
+//        // Sum of two Number Program
+//        System.out.print("Enter First Number : ");
+//        int n1 = sc.nextInt();
+//        System.out.println("Enter Second Number : ");
+//        int n2 = sc.nextInt();
+//
+//        int sum = n1 + n2;
+//        System.out.println("Sum of "+n1+" and "+n2+" is "+sum);
+
+
+
+    }
+}
