014f913739fd6ab3a2c6584c1a537dbbc4785ab0
deva964 <aryanraval299@gmail.com>
2025-05-12 12:52:32 +0530

    Literals

--- 2_DataType_Variable_Literal/Variables.java
+++ 2_DataType_Variable_Literal/Variables.java
@@ -4,8 +4,13 @@ public class Variables {
         //b = 128 ; error : possible lossy conversion int to byte
         System.out.println(b);
         short s = 300;
-        int i = 100000;
-        char c = 'A';
-        
+        int i = 214748;
+        Character c = 'A';
+
+        int x = 0b10101010; // binary form
+        int y = 012; // Octal form
+        int z = 0XA;
+        System.out.println(x+".."+y +".."+z);
+
     }
 }
