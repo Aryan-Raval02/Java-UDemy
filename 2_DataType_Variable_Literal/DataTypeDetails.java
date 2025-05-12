8cdf20d856be35b728cc55672dda1ac3d54afdd9
deva964 <aryanraval299@gmail.com>
2025-05-12 14:35:58 +0530

    DataTypeDetails Class

---
+++ 2_DataType_Variable_Literal/DataTypeDetails.java
@@ -0,0 +1,8 @@
+public class DataTypeDetails {
+    public static void main(String[] args) {
+        int x = 5;
+        int X = -5;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(X));
+    }
+}
