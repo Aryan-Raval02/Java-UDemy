d50da0cfb22e5ba1e952a3ac2abf2fa912b0eb6b
deva964 <aryanraval299@gmail.com>
2025-05-12 12:29:27 +0530

    Variables

---
+++ 2_DataType_Variable_Literal/DataType.java
@@ -0,0 +1,23 @@
+
+
+public class DataType {
+    public static void main(String[] args){
+        
+        System.out.println("Byte Min : "+Byte.MIN_VALUE);
+        System.out.println("Byte Max : "+Byte.MAX_VALUE);
+        System.out.println("Byte Bytes : "+Byte.BYTES);
+
+        System.out.println("Short Min : "+Short.MIN_VALUE);
+        System.out.println("Short Max : "+Short.MAX_VALUE);
+        System.out.println("Short Bytes : "+Short.BYTES);
+
+        System.out.println("Integer Min : "+Integer.MIN_VALUE);
+        System.out.println("Integer Max : "+Integer.MAX_VALUE);
+        System.out.println("Integer Bytes : "+Integer.BYTES);
+
+        System.out.println("Character Min : "+(int) Character.MIN_VALUE);
+        System.out.println("Character Max : "+(int) Character.MAX_VALUE);
+        System.out.println("Character Bytes : "+Character.BYTES);
+
+    }
+}
---
+++ 2_DataType_Variable_Literal/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 120;
+        //b = 128 ; error : possible lossy conversion int to byte
+        System.out.println(b);
+        short s = 300;
+        int i = 100000;
+        char c = 'A';
+        
+    }
+}
