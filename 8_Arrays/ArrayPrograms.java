2b8add15ecd2b93f71305c09291f073d36197865
deva964 <aryanraval299@gmail.com>
2025-05-18 15:48:34 +0530

    Arrays Programs Completed

--- 8_Arrays/ArrayPrograms.java
+++ 8_Arrays/ArrayPrograms.java
@@ -11,7 +11,10 @@ public class ArrayPrograms {
         findSecondLargestInArray(new int[]{-11,-56,-34,-2,0});
         rotateArrayToLeft(new int[]{1,2,3,4,5,6,7,8,9,0});
         rotateArrayToRight(new int[]{1,2,3,4,5,6,7,8,9,0});
-
+        copyArray(new int[]{23,34,45,5,6,6,7,7,8,8});
+        copyReverseArray(new int[]{23,34,45,5,6,6,7,7,8,8});
+        addMatrix();
+        multiplyMatrix();
     }
 
     public static void sumOfArrayElemenats(int[] nums){
@@ -87,5 +90,74 @@ public class ArrayPrograms {
         }
         nums[0] = temp;
         System.out.println("After Rotate To Right : "+Arrays.toString(nums));
+    }
+
+    public static void copyArray(int[] nums){
+        int[] b = new int[nums.length];
+
+        for(int i=0 ; i<nums.length ; i++){
+            b[i] = nums[i];
+        }
+
+        System.out.println("Copied Array : "+Arrays.toString(b));
+    }
+
+    public static void copyReverseArray(int[] nums){
+        int[] b = new int[nums.length];
+        int counter=0;
+
+        for(int i=nums.length-1 ; i>=0 ; i--){     // for(int i=nums.length-1,j=0 ; i>=0 ; i--,j++){
+            b[counter] = nums[i];                  //     b[j] = nums[i];
+            if(i == 0) continue;                   // }
+            else counter++;
+        }
+
+        System.out.println("Copied Reverse Array : "+Arrays.toString(b));
+    }
+
+    public static void addMatrix(){
+        int[][] arr1 = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] arr2 = {{9,8,7},{6,5,4},{3,2,1}};
+
+        int[][] ans = new int[3][3];
+
+        for(int i=0 ; i<arr1.length ; i++){
+            for(int j=0 ; j<arr2.length ; j++){
+                ans[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        printMatrix(arr1);
+        printMatrix(arr2);
+        printMatrix(ans);
+
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for(int[] x : matrix){
+            for(int y : x){
+                System.out.print(y+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void multiplyMatrix(){
+        int[][] arr1 = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] arr2 = {{1,0,0},{0,1,0},{0,0,1}};
+
+        int[][] ans = new int[3][3];
+
+        for(int i=0 ; i<arr1.length ; i++){
+            for(int j=0 ; j<arr2.length ; j++){
+                for(int k=0 ; k<arr2.length ; k++){
+                    ans[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+
+        printMatrix(arr1);
+        printMatrix(arr2);
+        printMatrix(ans);
     }
 }
