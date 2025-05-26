6ff891edba1cb540ccc761488a12dace0d54ef7a
deva964 <aryanraval299@gmail.com>
2025-05-26 19:48:49 +0530

    Packages and Access Modifier

---
+++ 17_Packages/Packages.java
@@ -0,0 +1,23 @@
+package pack1;
+
+class Demo
+{
+    void display()
+    {
+        System.out.println("Demo1");
+    }
+}
+class Demo2
+{
+    void display()
+    {
+        System.out.println("Demo2");
+    }
+}
+
+public class Packages
+{
+    public static void main(String[] args) {
+        
+    }
+}
